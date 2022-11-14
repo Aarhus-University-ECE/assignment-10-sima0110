b93bcca4d5e71af6a42d1676c9f12df3513b32a0
sima0110 <114432467+sima0110@users.noreply.github.com>
2022-11-14 19:32:52 +0100

    final

--- src/btree.c
+++ src/btree.c
@@ -7,31 +7,74 @@
 
 struct tree_node *Insert(int x, struct tree_node *t) {
   // Insert item x into the tree t
-
-  return NULL;
+  if (t == NULL){
+  
+  struct tree_node *new = malloc(sizeof(struct tree_node)); //creating a new node//
+  new->left = NULL; 
+  new->right = NULL; 
+  new->item = x; 
+  t = new;
+  } 
+  else if (x>t->item){ //goes to the right if x>t>item//
+    t->right = Insert(x,t->right);
+  } 
+  else{
+    t->left = Insert(x,t->left); //otherwise it goes to the left//
+  }
+  return t;
 }
 
 struct tree_node *Remove(int x, struct tree_node *t) {
   // Remove one item from the tree t
-
+ if(t == NULL){ //if the tree is empty// 
+  return t; 
+ }
+ else if(x < t->item){
+  t->left = Remove(x,t->left);
+  }
+ else if (x > t->item){
+  t->right = Remove(x,t->right);
+ }
+ if (t->left == NULL && t->right == NULL){
+ free(t); //frees the node if it is a leaf//
+ }
+ else if (x == t->item) 
+  {
+    t = NULL;
+  }
   return NULL;
 }
+
 
 int Contains(int x, struct tree_node *t) {
 
   // Return true if the tree t contains item x. Return false otherwise.
-
-  return NULL;
+  if (t = NULL){
+    return false; //returns false if the element is not found//
+  }
+  if (t->item == x){
+    return true; //returns true if the element is found//
+  } 
+  else if(t->item > x){
+    return Contains(x,t->left); 
+  }
+  else if(t->item <= x){
+    return Contains(x,t->right);
+  }
 }
 
 struct tree_node *Initialize(struct tree_node *t) {
   // Create an empty tree
-  return NULL;
+  t = NULL; //creates an empty tree// 
+  return t; 
 }
 
 int Empty(struct tree_node *t) {
   // Test if empty
-  return 0;
+  if(t == NULL){ //checks if the tree is empty//
+    return 1; //returns true if the tree is empty//
+  }
+  return 0; //otherwise it returns false//
 }
 
 int Full(struct tree_node *t) {
--- src/linked_list.c
+++ src/linked_list.c
@@ -26,21 +26,35 @@ void free_list(node *p) {
 void print_list(node *p) {
   // Add your code for exercise 1
   // There is NO testcode for this
+  if (p == NULL) //base case// 
+  return; 
+  else
+  {
+  printf("%d", p->value); 
+  print_list(p -> next); 
+  }
 }
 
 int sum_squares(node *p) {
   // Add your code for excercise 2
   // You can find the tests in tests.cpp
-  return -1;
+  if (p == NULL) //base case// 
+  return 0; 
+  else 
+  return p->value * p->value + sum_squares(p->next);
 }
 
 typedef int (*fn_int_to_int)(int);
 
 node *map(node *p, fn_int_to_int f) { 
   // Add your code for excercise 3
-  
+  if (p == NULL) //base case// 
   return NULL; 
-}
+  node *temp = malloc(sizeof(node)); //new node used to make a new list// 
+  temp->value = f(p->value); 
+  temp->next = map(p->next, f); 
+  return temp; 
+  }
 
 int square(int x) { return x * x; }
 
